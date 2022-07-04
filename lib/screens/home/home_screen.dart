052b03640168d2cd46d861b19e00684e2e0f746a
Leandro Sales <leandrossales.96@gmail.com>
2022-07-04 12:00:28 -0300

    finish layout for test

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:e2e_tests/screens/home/home_controller.dart';
+import 'package:e2e_tests/widgets/card_counter_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final HomeController controller = HomeController();
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: ValueListenableBuilder(
+            valueListenable: controller.title,
+            builder: (context, String title, _) => Text(
+                  title,
+                  key: const Key('title'),
+                )),
+      ),
+      body: FractionallySizedBox(
+        widthFactor: 1,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: TextFormField(
+                key: const Key('titleInput'),
+                initialValue: controller.title.value,
+                onChanged: (value) => controller.title.value = value,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text('Título'),
+                ),
+              ),
+            ),
+            CardCounter(
+              counter: controller.counter,
+            )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => controller.counter.value++,
+        label: const Text('Incrementar'),
+        icon: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,22 +5,21 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:e2e_tests/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:e2e_tests/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.plus_one));
     await tester.pump();
 
     // Verify that our counter has incremented.
